66bb8c78fc8be4b1976d3a52adb380f0b77d612e
gottu91 <gotuu91.afl@gmail.com>
2022-11-20 22:56:12 +0900

    render部分をReact18の書き方に変更しました。

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import { render } from "react-dom";
+// React v18
 import {App} from "./App";
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+
+// React ~v17
+// import { render } from "react-dom";
+// import {App} from "./App";
+// const rootElement = document.getElementById('root');
+// render(<App />, rootElement);
